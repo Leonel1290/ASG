b0ee2eb8d920a667b0a79fc393af325d2c7c1688
Leonel <leonel.sanchez@alumnos.itr3.edu.ar>
2025-05-13 10:13:48 -0300

    debug

--- app/Views/inicio.php
+++ app/Views/inicio.php
@@ -217,7 +217,7 @@
 </main>
 
 <footer>
-    <p>&copy; 2024 AgainSafeGas Solutions | Todos los derechos reservados.</p>
+    <p>&copy; 2025 AgainSafeGas Solutions | Todos los derechos reservados.</p>
 </footer>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
