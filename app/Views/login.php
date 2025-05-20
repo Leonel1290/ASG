c93d14d6cdb9131bcc29b02e908e0d68afdb9c3d
Leonel1290 <leonelsanchez@alumnos.itr3.edu.ar>
2025-05-20 13:40:55 -0300

    Update login.php

--- app/Views/login.php
+++ app/Views/login.php
@@ -1,15 +1,19 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Custom CSS -->
     <link rel="stylesheet" href="<?= base_url('css/login.css') ?>">
     <link rel="shortcut icon" href="<?= base_url('/imagenes/Logo.png'); ?>">
-    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <title>Login</title>
+
+    <link rel="manifest" href="<?= base_url('manifest.json') ?>">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="apple-mobile-web-app-title" content="ASG">
+    <link rel="apple-touch-icon" href="<?= base_url('imagenes/Logo.png') ?>">
+
   </head>
   <body>
     <div class="container">
@@ -18,7 +22,6 @@
           <div class="text-center mb-4">
             <h1 class="h3">Login</h1>
           </div>
-          <!-- Mostrar mensaje de error si existe -->
           <?php if (session()->get('error')): ?>
         <div>
             <p style="color: red;"><?= session()->get('error') ?></p>
@@ -27,19 +30,13 @@
 
           <form action="<?php echo base_url('/login') ?>" method="POST">
             <div class="form-group">
-              <label for="nombre">Nombre</label>
-              <input type="text" name="nombre" id="nombre" class="form-control" required>
+              <label for="email">Correo Electrónico</label>
+              <input type="email" class="form-control" id="email" name="email" placeholder="Ingrese su correo" required>
             </div>
-            <div class="form-group">
-              <label for="email">Email</label>
-              <input type="email" name="email" id="email" class="form-control" required>
-            </div>
-            <div class="form-group">
-              <label for="password">Password</label>
-              <input type="password" name="password" id="password" class="form-control" required>
-            </div>
-            <div class="ver">
-              <img src="https://static.thenounproject.com/png/1035969-200.png" id="eyeicon">
+            <div class="form-group password-container">
+              <label for="password">Contraseña</label>
+              <input type="password" class="form-control" id="password" name="password" placeholder="Ingrese su contraseña" required>
+              <img src="https://icon-library.com/images/eye-icon-png/eye-icon-png-1035969-200.png" id="eyeicon">
             </div>
             <button type="submit" class="btn btn-primary btn-block">Ingresar</button>
           </form>
@@ -50,8 +47,6 @@
         </div>
       </div>
     </div>
-    <!-- Optional JavaScript; choose one of the two! -->
-    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
     
@@ -60,13 +55,27 @@
      eyeicon.onclick = function(){
         if(password.type == "password"){
             password.type = "text";
-            eyeicon.src= "https://icons.veryicon.com/png/o/miscellaneous/myfont/eye-open-4.png";
-        }
-        else{
+            eyeicon.src = "https://cdn-icons-png.flaticon.com/512/9675/9675660.png";
+        }else{
             password.type = "password";
-            eyeicon.src= "https://static.thenounproject.com/png/1035969-200.png";
+            eyeicon.src = "https://icon-library.com/images/eye-icon-png/eye-icon-png-1035969-200.png";
         }
      }
-     </script>
-  </body>
+    </script>
+
+    <script>
+        if ('serviceWorker' in navigator) {
+            window.addEventListener('load', () => {
+                navigator.serviceWorker.register('<?= base_url('service-worker.js') ?>')
+                    .then(registration => {
+                        console.log('ServiceWorker registrado con éxito:', registration.scope);
+                    })
+                    .catch(error => {
+                        console.log('Fallo el registro de ServiceWorker:', error);
+                    });
+            });
+        }
+    </script>
+
+</body>
 </html>
