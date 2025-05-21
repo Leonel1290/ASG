d8dc3869be8e37fef707b2954279c784cfc5d65e
Leonel <leonel.sanchez@alumnos.itr3.edu.ar>
2025-05-21 13:22:02 -0300

    okk

--- app/Views/comprar.php
+++ app/Views/comprar.php
@@ -97,7 +97,6 @@
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <meta name="apple-mobile-web-app-title" content="ASG">
-    <link rel="apple-touch-icon" href="<?= base_url('imagenes/Logo.png') ?>">
 
 </head>
 <body>
