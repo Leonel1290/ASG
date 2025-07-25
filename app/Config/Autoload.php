4262f9e69807ce81fb0c4ecd1c5c670de9d35e38
manuel urrutia <manuelurrutia119@gmail.com>
2025-07-25 11:25:01 -0300

    sad

--- app/Config/Autoload.php
+++ app/Config/Autoload.php
@@ -13,47 +13,8 @@ class Autoload extends AutoloadConfig
      */
     public $psr4 = [
         APP_NAMESPACE => APPPATH,
-        'Config'      => APPPATH . 'Config', // Es buena práctica tener esta línea
-        'App\Libraries\Validation' => APPPATH . 'Libraries/Validation', // <-- ¡Añade esta línea!
-    ];
-
-    /**
-     * -------------------------------------------------------------------
-     * Class Map
-     * -------------------------------------------------------------------
-     */
-    public $classmap = [];
-
-    /**
-     * -------------------------------------------------------------------
-     * Files
-     * -------------------------------------------------------------------
-     */
-    public $files = [];
-
-    /**
-     * -------------------------------------------------------------------
-     * Helpers
-     * -------------------------------------------------------------------
-     */
-    public $helpers = [];
-}<?php
-
-namespace Config;
-
-use CodeIgniter\Config\AutoloadConfig;
-
-class Autoload extends AutoloadConfig
-{
-    /**
-     * -------------------------------------------------------------------
-     * Namespaces
-     * -------------------------------------------------------------------
-     */
-    public $psr4 = [
-        APP_NAMESPACE => APPPATH,
-        'Config'      => APPPATH . 'Config', // Es buena práctica tener esta línea
-        'App\Libraries\Validation' => APPPATH . 'Libraries/Validation', // <-- ¡Añade esta línea!
+        'Config'      => APPPATH . 'Config',
+        'App\Libraries\Validation' => APPPATH . 'Libraries/Validation',
     ];
 
     /**
