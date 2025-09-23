50cfd280c6b44800f25b84aef15794a00115c16c
Leonel <leonel.sanchez@alumnos.itr3.edu.ar>
2025-09-23 13:45:47 -0300

    logo arriba

--- app/Views/inicio.php
+++ app/Views/inicio.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>ASG - Seguridad en tu Hogar</title>
-    
+    <link rel="shortcut icon" href="<?= base_url('/imagenes/Logo.png'); ?>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
