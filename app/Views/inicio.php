e1970a98eefa409f9c0ff0619bb767ccad6f4d39
manuel <manuelurrutia119@gmail.com>
2025-10-24 09:32:15 -0300

    sdf

--- app/Views/inicio.php
+++ app/Views/inicio.php
@@ -404,10 +404,10 @@
                     </p>
                     <div class="d-flex gap-3 flex-wrap" data-aos="fade-up" data-aos-delay="400">
                         <a href="<?= base_url('/loginobtener') ?>" class="btn btn-custom">
-                            <i class="fas fa-shopping-cart me-1"></i> Iniciar Sesión
+                            <i class="fad fa-sign-in"></i> Iniciar Sesión
                         </a>
                         <a href="<?= base_url('/comprar') ?>" class="btn btn-outline-custom">
-                            <i class="fas fa-info-circle me-1"></i> Comprar Ahora
+                            <i class="fas fa-shopping-cart me-1"></i> Comprar Ahora
                         </a>
                     </div>
                 </div>
