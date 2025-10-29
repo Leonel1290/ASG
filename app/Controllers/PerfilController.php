10e48aca5fdb932dd894f607b7a2ea039cdeb791
manuel <manuelurrutia119@gmail.com>
2025-10-29 10:35:22 -0300

    asd

--- app/Controllers/PerfilController.php
+++ app/Controllers/PerfilController.php
@@ -50,7 +50,7 @@ class PerfilController extends BaseController
 
         // Obtener dispositivos enlazados por el usuario
         $data['dispositivosEnlazados'] = $this->dispositivoModel
-                                            ->where('usuario_id', $usuarioId) // <--- CORRECCIÓN CLAVE
+                                            ->where('id_usuario', $usuarioId) E
                                             ->findAll();
 
         log_message('debug', 'PerfilController::index() - Dispositivos encontrados: ' . count($data['dispositivosEnlazados']));
